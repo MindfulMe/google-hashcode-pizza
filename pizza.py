823ec5bc8d2efdef43f99e57c7136dd43cc0fef0
Avo <go.go.gg.rostislav@gmail.com>
2018-06-04 19:42:34 +0300

    main function with first raw parser

--- pizza.py
+++ pizza.py
@@ -4,3 +4,21 @@ import sys
 
 
 def read_file(filename):
+    '''
+    Read the file.
+    Return the pizza as a binary matrix: tomatoes = 1, mushrooms = 0 + returns the minimum ingredients required for each slice,
+    and the maximum cells allowed for each slice // 'tis not fully mine description
+    '''
+    
+    with open(filename, 'r') as f:
+        line = f.readline()
+        rows, cols, min_ings, max_cells = [int(n) for n in line.split()] '''get first row '''
+
+def main():
+    '''
+    Main function
+    '''
+
+
+if __name__ == '__main__':
+    main()
